0ea1fa99f9bf3afbcb92f177ae8b7a8a270ecfc1
vijai <vijai.djearam@gmail.com>
2023-11-22 14:16:21 +0100

    tsets

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from pyscript import document
 def submit(event):
     lang = document.querySelector("#lang")
     language = lang.value
-    output_div = document.querySelector("#output1")
+    output_div = document.querySelector("#output")
     output_div.innerText = language
 
