3bd620e16cc9249192cf9bb1107ff31a93f76c76
vijai <vijai.djearam@gmail.com>
2023-11-22 13:59:47 +0100

    sdfsd

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import arrr
 from pyscript import document
 
 
-def translate_english(event):
-    input_text = document.querySelector("#english")
-    english = input_text.value
-    output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
+def submit(event):
     lang = document.querySelector("#lang")
     language = lang.value
     output_div = document.querySelector("#output1")
